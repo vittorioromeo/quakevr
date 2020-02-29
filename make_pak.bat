0fe9db08da8b3c578aa71c9f510644000dfaeecf
Vittorio Romeo <vittorio.romeo@outlook.com>
2020-02-29 23:40:09 +0000

    Fix paths, README WIP

--- make_pak.bat
+++ make_pak.bat
@@ -1,5 +1,5 @@
 F:
-cd F:\stuf\Quakespasm-OpenVR\QC
+cd F:\stuf\quakevr\QC
 .\frikqcc.exe -summary -nolog -nopause -Ot -Oi -Op -Oc -Od -Os -Ol -On -Of -Ou -Oo -Or -Oa +warn 2 -cos
 pak.exe -c -v pak3.pak progs.dat
-cp F:\stuf\Quakespasm-OpenVR\QC\pak3.pak F:\stuf\Quakespasm-OpenVR\Windows\VisualStudio\Build-quakespasm-sdl2\x64\Debug\Id1
+cp F:\stuf\quakevr\QC\pak3.pak F:\stuf\quakevr\Windows\VisualStudio\Build-quakespasm-sdl2\x64\Debug\Id1
