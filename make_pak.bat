90fabb1a82523a6732ada4de690d0099d9e1efa4
Vittorio Romeo <vittorio.romeo@outlook.com>
2020-05-01 10:52:29 +0100

    update make_pak

--- make_pak.bat
+++ make_pak.bat
@@ -3,6 +3,6 @@ cd C:\OHWorkspace\quakevr\QC
 .\fteqcc64.exe -O3 -Fautoproto -progdefs -Olo -Fiffloat -Fifvector -Fvectorlogic
 .\pak.exe -c -v pak11.pak progs.dat
 xcopy /y C:\OHWorkspace\quakevr\QC\pak11.pak C:\OHWorkspace\quakevr\ReleaseFiles\Id1
-echo F | xcopy /y C:\OHWorkspace\quakevr\QC\progdefs.h C:\OHWorkspace\quakevr\Quake\progdefs_generated.hpp
+:: echo F | xcopy /y C:\OHWorkspace\quakevr\QC\progdefs.h C:\OHWorkspace\quakevr\Quake\progdefs_generated.hpp
 
 :: xcopy /y C:\OHWorkspace\quakevr\QC\pak11.pak C:\OHWorkspace\quakevr\Windows\VisualStudio\Build-quakespasm-sdl2\x64\Debug\Id1
