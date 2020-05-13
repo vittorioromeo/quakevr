481f8fe6c76660ca1e2f89304ab59d71b44ddef7
Vittorio Romeo <vittorio.romeo@outlook.com>
2020-05-13 14:57:56 +0100

    Formatting

--- make_pak.bat
+++ make_pak.bat
@@ -2,7 +2,7 @@ C:
 cd C:\OHWorkspace\quakevr\QC
 
 :: definitely crashes
-:: .\fteqcc64.exe -O3 -Fautoproto -progdefs -Olo -Fiffloat -Fifvector -Fvectorlogic
+.\fteqcc64.exe -O3 -Fautoproto -progdefs -Olo -Fiffloat -Fifvector -Fvectorlogic
 
 :: definitely crashes
 :: .\fteqcc64.exe -O1 -Fautoproto -progdefs -Olo -Fiffloat -Fifvector -Fvectorlogic
@@ -10,7 +10,8 @@ cd C:\OHWorkspace\quakevr\QC
 :: definitely crashes
 :: .\fteqcc64.exe -O1 -Fautoproto -progdefs -Olo
 
-.\fteqcc64.exe -O3 -Fautoproto -progdefs -Ono-lo -Fiffloat -Fifvector -Fvectorlogic
+:: doesn't seem to crash - best version
+:: .\fteqcc64.exe -O3 -Fautoproto -progdefs -Ono-lo -Fiffloat -Fifvector -Fvectorlogic
 
 :: doesn't seem to crash
 ::.\fteqcc64.exe -O1 -Fautoproto -progdefs -Fiffloat -Fifvector -Fvectorlogic
