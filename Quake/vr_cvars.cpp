94c05dcef90bb767d7535d0474bf1b5d21f56318
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-02-23 02:38:48 +0000

    beta release

--- Quake/vr_cvars.cpp
+++ Quake/vr_cvars.cpp
@@ -183,7 +183,7 @@ DEFINE_FCVAR_ARCHIVE(vr_autosave_show_message, 0);
 DEFINE_FCVAR_ARCHIVE(vr_finger_blending, 1);
 DEFINE_FCVAR_ARCHIVE(vr_finger_blending_speed, 50);
 DEFINE_FCVAR_ARCHIVE(vr_menu_mouse_pointer_hand, 1);
-DEFINE_FCVAR_ARCHIVE(vr_reload_mode, 1);
+DEFINE_FCVAR_ARCHIVE(vr_reload_mode, 2);
 DEFINE_FCVAR_ARCHIVE(vr_show_weapon_text, 1);
 DEFINE_FCVAR_ARCHIVE(vr_disablehaptics, 0);
 
