35b33b6c63a9d29dd2855e2d47adea6f58436be3
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-03-25 00:51:31 +0000

    Increase packet sizes

--- Quake/quakedef_macros.hpp
+++ Quake/quakedef_macros.hpp
@@ -88,12 +88,13 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
     (0.03125f) // 1/32 epsilon to keep floating point happy (moved from world.c)
 
 #define MAX_MSGLEN \
-    96000 // max length of a reliable message //ericw -- was 32000
+    128000 // max length of a reliable message //ericw -- was 32000
+
 #define MAX_DATAGRAM \
-    64000 // max length of unreliable message //johnfitz -- was 1024
+    128000 // max length of unreliable message //johnfitz -- was 1024
 
 #define DATAGRAM_MTU \
-    1400 // johnfitz -- actual limit for unreliable messages to nonlocal clients
+    2048 // johnfitz -- actual limit for unreliable messages to nonlocal clients
 
 //
 // per-level limits
